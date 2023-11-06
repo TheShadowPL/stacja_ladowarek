9f7302a08dade072e24850e2c0c3c5e9f8fb7b67
Krystian Tomczyk <krynio340@gmail.com>
2023-11-06 15:59:34 +0100

    added index.blade.php and json with chargers

---
+++ stacja_ladowarek/resources/views/index.blade.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/app.css">
+    <style>
+    @import url('https://fonts.googleapis.com/css2?family=Lexend+Deca:wght@300;500;700&display=swap');
+    </style>
+    <title>Charger</title>
+</head>
+<body>
+    <section class="filters">
+        <div class="title">
+            <h1>Filtry</h1></div>
+        <div class="input">
+            <div class="input-title">
+                Lokalizacja
+            </div>
+            <input type="text" placeholder="Jelenia góra">
+        </div>        
+        <div class="input">
+            <div class="input-title">
+                Marka ładowarki
+            </div>
+            <input type="text" placeholder="Samsung">
+        </div>        
+        <div class="input">
+            <div class="input-title">
+                Dostępność
+            </div>
+            <input type="text" placeholder="Dostępne">
+        </div>
+    </section>    
+    <section class="container">
+        <div class="title"><h1>Ładowarki</h1></div>
+    </section>
+</body>
+</html>
